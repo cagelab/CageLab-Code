b0c0c8920fdf23f216fa33a9633691000b637467
iandol <iandol@cog5.org>
2026-05-25 11:44:40 +0800

    feat: use pixi to install tmux dependency

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# CageLab Bootstrap Script: This script is designed to set up a new machine
+# with all the necessary software and configurations for the CageLab
+# environment. It will install essential packages, set up MATLAB, configure
+# system services, and clone the necessary repositories from our codebase.
+# The script is intended to be run on a fresh installation of Linux (Ubuntu)
+# or macOS, and it will ensure that the machine is ready for development and
+# research work in the CageLab environment.
+
 export PLATFORM=$(uname)
 export ARCH=$(uname -m)
 export SPATH="$HOME/Code/CageLab-Code"
@@ -20,7 +29,9 @@ printf '\e[36m'
 printf "Using %s...\n" "$PLATFORM"
 printf '\e[0m'
 
-# Create some folders
+#================================================= Create some folders also
+# make sure we have permissions to write to /usr/local/bin and
+# /usr/local/etc for later steps
 mkdir -p "$HOME/Code"
 mkdir -p "$HOME/bin"
 mkdir -p "$HOME/.local/bin"
@@ -29,22 +40,65 @@ mkdir -p "$HOME/.config/tmuxp"
 sudo chown -R "$USER":"$USER" /usr/local/bin
 sudo chown -R "$USER":"$USER" /usr/local/etc
 
-# this section disables the touch screen at start
-# replace with name of the touch panel device
+#================================================= Setup toggleInput service for touch panel
+# replace "NAME" with name of the touch panel device
 # you can find the name by running `xinput list` in a terminal
 # and looking for the device that corresponds to your touch panel
-# CHANGE name TO YOUR TOUCH PANEL NAME
-name="ILITEK-TP"
-sd '^(ExecStart=\/usr\/local\/bin\/toggleInput [^ ]+ ).*$' '$1"'$name'"' "$SPATH/services/toggleInput.service"
-sudo ln -svf "$SPATH/config/toggleInput" /usr/local/bin/toggleInput &&
-	sudo chmod +x /usr/local/bin/toggleInput &&
-	sudo ln -svf "$SPATH/services/toggleInput.service" "$HOME/.config/systemd/user" &&
-	systemctl --user daemon-reload &&
-	systemctl --user enable toggleInput.service &&
-	systemctl --user start toggleInput.service
+# CHANGE NAME TO YOUR TOUCH PANEL NAME
+if [ "$PLATFORM" = "Linux" ]; then
+	NAME="ILITEK-TP"
+	sd '^(ExecStart=\/usr\/local\/bin\/toggleInput [^ ]+ ).*$' '$1"'$NAME'"' "$SPATH/services/toggleInput.service"
+	sudo ln -svf "$SPATH/config/toggleInput" /usr/local/bin/toggleInput &&
+		sudo chmod +x /usr/local/bin/toggleInput &&
+		sudo ln -svf "$SPATH/services/toggleInput.service" "$HOME/.config/systemd/user" &&
+		systemctl --user daemon-reload &&
+		systemctl --user enable toggleInput.service &&
+		systemctl --user start toggleInput.service
+fi
+
+#============================================== APT + snap + flatpak packages
+if [ "$PLATFORM" = "Linux" ]; then
+	sudo apt --fix-broken install
+	sudo apt update && sudo apt -y full-upgrade
+	sudo apt -my install apt-transport-https ca-certificates software-properties-common
+	sudo apt -my install build-essential zsh git gparted vim curl file mc
+	sudo apt -my install gawk mesa-utils exfatprogs
+	sudo apt -my install freeglut3-dev 
+	sudo apt -my install libglut-dev
+	sudo apt -my install pipewire-pulse pulseaudio-utils
+	sudo apt -my install openssh-server
+	sudo apt -my install i3 rofi nitrogen xdotool
+	sudo apt -my install p7zip-full p7zip-rar figlet jq htop
+	sudo apt -my install libunrar5 libdc1394-25 libraw1394-11
+	sudo apt -my install gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+	sudo apt -my install synaptic zathura zathura-pdf-poppler zathura-ps
+	sudo apt -my install tmux tmuxp
+	sudo apt -my install snapd
+	sudo apt -my install openjdk-21-jre
+	sudo apt -my install flatpak
+	sudo apt -my install wakeonlan etherwake
+	sudo apt -my install python3-pip python3-venv
+	
+	# get update i3 desktop manager
+	"$SPATH/setup/config/geti3.sh"
+
+	# Install snap packages vlc and VS Code
+	sudo snap install core
+	sudo snap install starship --edge
+	[[ ! -f $(which vlc) ]] && sudo snap install vlc
+	[[ ! -f $(which code) ]] && sudo snap install --classic code
+
+	# flatpak installs OBS
+	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+	flatpak install -y flathub com.obsproject.Studio
+	
+fi
 
 #================================================= Install Pixi (cross-platform package manager)
 [[ ! -d $HOME/.pixi/bin ]] && eval curl -fsSL https://pixi.sh/install.sh | bash
+
+#============================================ Get pixi to update itself and all global packages
+pixi self-update; pixi global sync; pixi global -v update
 
 #============================================== Install Netbird
 # Use the setup key from our password manager or do that later
@@ -69,44 +123,6 @@ if [[ $ans == 'y' ]]; then
 fi
 [[ -f $mpath ]] && ln -sfv /usr/local/MATLAB/$mversion/bin/matlab /usr/local/bin
 
-#============================================== APT + snap + flatpak packages
-if [ "$PLATFORM" = "Linux" ]; then
-	sudo apt --fix-broken install
-	sudo apt update && sudo apt -y full-upgrade
-	sudo apt -my install apt-transport-https ca-certificates software-properties-common
-	sudo apt -my install build-essential zsh git gparted vim curl file mc
-	sudo apt -my install gawk mesa-utils exfatprogs
-	sudo apt -my install freeglut3-dev 
-	sudo apt -my install libglut-dev
-	sudo apt -my install pipewire-pulse pulseaudio-utils
-	sudo apt -my install openssh-server
-	sudo apt -my install i3 rofi nitrogen xdotool
-	sudo apt -my install p7zip-full p7zip-rar figlet jq htop
-	sudo apt -my install libunrar5 libdc1394-25 libraw1394-11
-	sudo apt -my install gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
-	sudo apt -my install synaptic zathura zathura-pdf-poppler zathura-ps
-	sudo apt -my install tmux tmuxp
-	sudo apt -my install snapd
-	sudo apt -my install openjdk-21-jre
-	sudo apt -my install flatpak
-	sudo apt -my install wakeonlan etherwake
-	sudo apt -my install python3-pip python3-venv
-	sudo snap install core
-	sudo snap install starship --edge
-
-	# get update i3 desktop manager
-	"$SPATH/setup/config/geti3.sh"
-
-	# Install snap packages vlc and VS Code
-	[[ ! -f $(which vlc) ]] && sudo snap install vlc
-	[[ ! -f $(which code) ]] && sudo snap install --classic code
-
-	# flatpak installs OBS
-	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-	flatpak install -y flathub com.obsproject.Studio
-	
-fi
-
 #============================================== install or update cogmoteGO
 curl -sS https://raw.githubusercontent.com/Ccccraz/cogmoteGO/main/install.sh | sh &&
 	cogmoteGO service &&
@@ -118,13 +134,13 @@ curl -sS https://raw.githubusercontent.com/Ccccraz/cogmoteGO/main/install.sh | s
 	sudo dpkg -i "$HOME/Downloads/nomachine.deb"
 
 #=============================================== Install eget and get mediamtx and sunshine
+# eget is a small and fast package manager for binaries, we use it to get mediamtx
 [[ ! -f /usr/local/bin/eget ]] && curl https://zyedidia.github.io/eget.sh | sh && chmod +x eget && mv eget /usr/local/bin/eget
 [[ ! -f /usr/local/bin/mediamtx ]] && eget bluenviron/mediamtx --to=/usr/local/bin && ln -svf /usr/local/bin/mediamtx $HOME/.local/bin
 
 #============================================= Clone our core repos from gitee
 mkdir -p "$HOME/Code"
 cd "$HOME/Code" || exit
-[[ ! -d 'Setup' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/Setup.git
 [[ ! -d 'Psychtoolbox' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/Psychtoolbox.git
 [[ ! -d 'opticka' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/opticka.git
 [[ ! -d 'CageLab-Code' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/CageLab-Code.git
@@ -152,9 +168,6 @@ fi
 
 #============================================ Make sure all the symlinks are correct
 "$SPATH/setup/makelinks.sh"
-
-#============================================ Get pixi to update itself and all global packages
-pixi self-update; pixi global sync; pixi global -v update
 
 printf '\n\n--->>> All Done...\n'
 printf '\e[m'
